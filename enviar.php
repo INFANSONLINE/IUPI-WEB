85cdc1a68e2e3d70ac64e434ae201c810017c6f8
mac <mac@MacBook-Pro-de-mac.local>
2022-02-10 10:50:21 -0300

    first commit

---
+++ enviar.php
@@ -0,0 +1,11 @@
+<?php
+$destino="info@infansonline.com";
+$nombre=$_POST["name"];
+$mail=$_POST["Email"];
+$telefono=$_POST["phone"];
+$mensaje=$_POST["message"];
+$contenido="name:" . $nombre . "\nEmail:" . $mail . "\ntelefono:" . $phone . "\nmensaje:" . $mensaje;
+mail($destino, "Nueva entrevista", $contenido);
+header("Location:gracias.html");
+
+?>
