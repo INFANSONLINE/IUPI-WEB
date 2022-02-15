899140fbe1fdda2095a8fe15ebfa77a38865efa2
INFANSONLINE <mac@MacBook-Pro-de-mac.local>
2022-02-15 12:11:15 -0300

    actualizo

--- enviar.php
+++ enviar.php
@@ -5,7 +5,7 @@ $mail=$_POST["Email"];
 $telefono=$_POST["phone"];
 $mensaje=$_POST["message"];
 $contenido="name:" . $nombre . "\nEmail:" . $mail . "\ntelefono:" . $phone . "\nmensaje:" . $mensaje;
-mail($destino, "Nueva entrevista", $contenido);
+mail($destino,"Nueva entrevista", $contenido);
 header("Location:gracias.html");
 
 ?>
